6a2f5a91292b0514f6a6503266120e7fe944d41e
hanappe <hanappe@csl.sony.fr>
2020-09-23 23:20:39 +0200

    added error estimate

--- test/src/cnc_range_tests.cpp
+++ test/src/cnc_range_tests.cpp
@@ -202,3 +202,57 @@ TEST_F(cnc_range_tests, test_cnc_range_parse_set_max_lt_min)
         ASSERT_NE(err, 0);
 
 }
+
+TEST_F(cnc_range_tests, test_cnc_range_outofbounds_error)
+{
+        // Arrange
+        cnc_range_t range;
+
+        json_object_t r = json_parse("[[0,1],[0,1],[0,1]]");
+        
+        // Act
+        int err = cnc_range_parse(&range, r);
+        json_unref(r);
+        
+        double d1 = cnc_range_error(&range, 0, 0, 0);
+        double d2 = cnc_range_error(&range, 1, 0, 0);
+        double d3 = cnc_range_error(&range, 0, 1, 0);
+        double d4 = cnc_range_error(&range, 1, 1, 0);
+        double d5 = cnc_range_error(&range, 0, 0, 1);
+        double d6 = cnc_range_error(&range, 1, 0, 1);
+        double d7 = cnc_range_error(&range, 0, 1, 1);
+        double d8 = cnc_range_error(&range, 1, 1, 1);
+
+        double d9 = cnc_range_error(&range, 2, 0, 0);
+        double d10 = cnc_range_error(&range, 0, -1, 0);
+        double d11 = cnc_range_error(&range, 0, 2, 0);
+        double d12 = cnc_range_error(&range, 0, -1, 0);
+
+        double d13 = cnc_range_error(&range, 2, 2, 0);
+        double d14 = cnc_range_error(&range, 2, 2, 2);
+        
+        //Assert
+        ASSERT_EQ(err, 0);
+        ASSERT_EQ(d1, 0);
+        ASSERT_EQ(d2, 0);
+        ASSERT_EQ(d3, 0);
+        ASSERT_EQ(d4, 0);
+        ASSERT_EQ(d5, 0);
+        ASSERT_EQ(d6, 0);
+        ASSERT_EQ(d7, 0);
+        ASSERT_EQ(d8, 0);
+        
+        ASSERT_GT(d9, 0.99);
+        ASSERT_LT(d9, 1.01);
+        ASSERT_GT(d10, 0.99);
+        ASSERT_LT(d10, 1.01);
+        ASSERT_GT(d11, 0.99);
+        ASSERT_LT(d11, 1.01);
+        ASSERT_GT(d12, 0.99);
+        ASSERT_LT(d12, 1.01);
+
+        ASSERT_GT(d13, 1.4);
+        ASSERT_LT(d13, 1.5);
+        ASSERT_GT(d14, 1.7);
+        ASSERT_LT(d14, 1.8);
+}
