f4590108f3850b1a60e307739cb9a17e8ee8092f
Doug Boari <doug@tastymushroom.com>
2021-04-26 15:36:40 +0100

    Remove list.c & tests from libr. Refactor Quincunx.cpp to use std::vector

--- libromi/src/weeder/quincunx/point.h
+++ libromi/src/weeder/quincunx/point.h
@@ -24,20 +24,12 @@
 #ifndef _ROMI_POINT_H_
 #define _ROMI_POINT_H_
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 typedef struct _point_t {
         float x, y, z;
+        _point_t() = default;
+        _point_t(float x, float y, float z) : x(x), y(y), z(z){};
+
 } point_t;
 
-point_t *new_point(float x, float y, float z);
-void delete_point(point_t *p);
-void point_set(point_t *p, float x, float y, float z);
-        
-#ifdef __cplusplus
-}
-#endif
 
 #endif // _ROMI_POINT_H_
