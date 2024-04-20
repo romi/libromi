6544daed5ebb8080826611435d56a867932a7941
hanappe <peter@hanappe.com>
2024-04-20 12:43:45 +0200

    remote config

---
+++ libromi/include/camera/CameraFactory.h
@@ -0,0 +1,62 @@
+/*
+  romi-rover
+
+  Copyright (C) 2019 Sony Computer Science Laboratories
+  Author(s) Peter Hanappe
+
+  romi-rover is collection of applications for the Romi Rover.
+
+  romi-rover is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see
+  <http://www.gnu.org/licenses/>.
+
+ */
+
+#ifndef __ROMI_CAMERAFACTORY_H
+#define __ROMI_CAMERAFACTORY_H
+
+#include <memory>
+#include <rcom/ILinux.h>
+#include <rcom/ILog.h>
+#include "api/ICamera.h"
+#include "camera/ICameraInfo.h"
+#include "camera/ICameraInfoIO.h"
+
+namespace romi {
+        
+        class CameraFactory
+        {
+        protected:
+                static std::unique_ptr<ICamera> make_camera(
+                        std::shared_ptr<rcom::ILog>& rcomlog,
+                        ICameraSettings& settings);
+                static std::unique_ptr<ICamera> make_fake_camera(
+                        ICameraSettings& settings);
+                static std::unique_ptr<ICamera> make_external_camera(
+                        ICameraSettings& settings);
+                static std::unique_ptr<ICamera> make_remote_camera(
+                        std::shared_ptr<rcom::ILog>& rcomlog,
+                        ICameraSettings& settings);
+                
+        public:
+                virtual ~CameraFactory() = default;
+
+                static std::unique_ptr<ICamera> create(
+                        rcom::ILinux& linux,
+                        std::shared_ptr<rcom::ILog>& rcomlog,
+                        std::shared_ptr<ICameraInfoIO>& io);
+                
+        };
+}
+
+#endif // __ROMI_CAMERAFACTORY_H
---
+++ libromi/src/camera/CameraFactory.cpp
@@ -0,0 +1,114 @@
+/*
+  romi-rover
+
+  Copyright (C) 2019 Sony Computer Science Laboratories
+  Author(s) Peter Hanappe
+
+  romi-rover is collection of applications for the Romi Rover.
+
+  romi-rover is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see
+  <http://www.gnu.org/licenses/>.
+
+ */
+
+#include <stdexcept>
+#include <rcom/RcomClient.h>
+#include "util/Logger.h"
+#include "camera/FakeCamera.h"
+#include "camera/ExternalCamera.h"
+#include "camera/CameraWithConfig.h"
+#include "rpc/RemoteCamera.h"
+#include "camera/CameraFactory.h"
+
+namespace romi {
+
+        std::unique_ptr<ICamera> CameraFactory::create(
+                rcom::ILinux&,
+                std::shared_ptr<rcom::ILog>& rcomlog,
+                std::shared_ptr<ICameraInfoIO>& io)
+        {
+                r_debug("CameraFactory::create");
+                
+                auto info = io->load();
+                
+                // Camera
+                std::unique_ptr<ICamera> real_camera
+                        = make_camera(rcomlog, info->get_settings());
+                std::unique_ptr<ICamera> camera
+                        = std::make_unique<CameraWithConfig>(io, real_camera);
+
+                return camera;
+        }
+
+        std::unique_ptr<ICamera> CameraFactory::make_camera(
+                std::shared_ptr<rcom::ILog>& rcomlog,
+                ICameraSettings& settings)
+        {
+                r_debug("CameraFactory::make_camera");
+                
+                if (settings.type() == "external-camera")
+                        return make_external_camera(settings);
+                        
+                else if (settings.type() == "remote-camera")
+                        return make_remote_camera(rcomlog, settings);
+                        
+                else if (settings.type() == "fake-camera")
+                        return make_fake_camera(settings);
+
+                else {
+                        throw std::runtime_error("Unknown camera type");
+                }
+        }
+        
+        std::unique_ptr<ICamera> CameraFactory::make_fake_camera(ICameraSettings&)
+        {
+                r_debug("CameraFactory::make_fake_camera");
+                
+                size_t width, height;
+                // get_resolution(settings, width, height);
+                width = 1920;
+                height = 1080;
+                
+                std::unique_ptr<ICamera> camera
+                        = std::make_unique<FakeCamera>(width, height);
+                return camera;
+        }
+
+        std::unique_ptr<ICamera> CameraFactory::make_external_camera(ICameraSettings& settings)
+        {
+                r_debug("CameraFactory::make_external_camera");
+                
+                std::string executable;
+                settings.get_option("executable", executable);
+                
+                std::unique_ptr<ICamera> camera
+                        = std::make_unique<ExternalCamera>(executable);
+                return camera;
+        }
+
+        std::unique_ptr<ICamera> CameraFactory::make_remote_camera(
+                std::shared_ptr<rcom::ILog>& rcomlog,
+                ICameraSettings& settings)
+        {
+                r_debug("CameraFactory::make_remote_camera");
+                
+                std::string topic;
+                settings.get_option("topic", topic);
+                
+                auto client = rcom::RcomClient::create(topic, 10.0, rcomlog);
+                std::unique_ptr<ICamera> camera
+                        = std::make_unique<RemoteCamera>(client);
+                return camera;
+        }
+}
